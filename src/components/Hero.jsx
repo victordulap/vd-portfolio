baa0365a21e0f6d7eebda17adb7b54d57c64d525
Victor Dulap <victordulapit@gmail.com>
2022-01-25 19:02:35 +0200

    add hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Hero = () => {
-  return <div className="container mx-auto h-screen">lorem123123</div>;
+  return (
+    <div className="container 2xl:px-96 xl:px-52 px-6 py-4 mx-auto h-screen flex flex-col justify-center">
+      <h6 className="font-secondary text-violet-400 mb-4">Hi, my name is</h6>
+      <h1 className="text-5xl font-semibold text-gray-100 mb-4">Victor Dulap</h1>
+      <h2 className="text-3xl font-semibold text-gray-300 mb-3">I build web applications.</h2>
+      <p className="text-gray-400">I'm a Front-end Web Developer based in Chișinău, Moldova. </p>
+    </div>
+  );
 };
 
 export default Hero;
